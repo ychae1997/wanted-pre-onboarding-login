1fd1822493543a5e8aa95772f0814aa27d0c13fd
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-10-13 19:51:26 +0900

    fe-p4 : sidebar 어드민 페이지 버튼 렌더링

--- fe-p4/client/src/components/Sidebar.tsx
+++ fe-p4/client/src/components/Sidebar.tsx
@@ -36,7 +36,7 @@ const Sidebar: React.FC<SidebarProps> = ({ sidebarContent, userProfile }) => {
         .filter((element ) => {
           // TODO 4-1: element.isAdminOnly가 true일 때
           //  admin user(userProfile.userInfo.roles에 'admin'이 포함됨)에게만 메뉴 보여주기
-          return true
+          return element.isAdminOnly ? userProfile?.userInfo.roles.includes(AdminRole) : !!userProfile
         })
         .map((element) => {
         return (<li
